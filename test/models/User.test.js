35f6bb5f23b98ce9f967863706305ae099299584
AliciaGaona <aligaovela@gmail.com>
2022-04-28 05:05:08 -0500

    se agrega prueba unitaria para requerimiento 4, se verifica su fallo

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -23,3 +23,12 @@ test('Add getters', () =>{
     expect(user.getDateCreated).not.toBeUndefined()
     expect(user.getLastUpdated).not.toBeUndefined()
 });
+
+test('Add setters', () =>{
+    const user = new User(1,"carlogilmar","Carlo","Bio")
+    user.setUserName="Gilmar"
+    expect(user.username).toBe("Gilmar")
+    user.setBio="New bio"
+    expect(user.bio).toBe("Bio")
+ 
+});
