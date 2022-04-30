589455a062832c84068c2380e23afdddd4ef0b8e
AliciaGaona <aligaovela@gmail.com>
2022-04-29 21:26:26 -0500

    Se crea método para qeu la prueba pase

--- app/services/UserService.js
+++ app/services/UserService.js
@@ -1,9 +1,8 @@
 const User =require('./../../app/models/User')
 
-
 class UserService{
  static create(id, username,name){
-     return new User (id. username, name, "Sin bio") 
+     return new User (id, username, name, "Sin bio") 
  }
 }
 
