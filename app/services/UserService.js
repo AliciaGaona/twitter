b8209c4ba96ef1e2b395299198abc96534531139
AliciaGaona <aligaovela@gmail.com>
2022-04-29 21:14:47 -0500

    Se crea método create, con comentarios de pasos

--- app/services/UserService.js
+++ app/services/UserService.js
@@ -1,5 +1,10 @@
+const User =require('./../../app/models/User') //indicampos ruta del modelo User a ocupar(require)
+
+
 class UserService{
- 
+ static create(id, username,name){//metodo estatico que solo recibe los parámetros del `id`, `username` y `name`
+     return new User (id. username, name, "Sin bio") //regrtesará un nuevo usuario objeto con los datos que recibe
+ }
 }
 
-module.exports=UserService
+module.exports=UserService// exporto clase
