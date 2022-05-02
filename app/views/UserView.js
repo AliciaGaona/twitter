cc14d3bcd729c38af09df94e854714bb6168481b
AliciaGaona <aligaovela@gmail.com>
2022-05-01 23:36:22 -0500

    Se valida que pasa prueba de requerimeinto 4, se comenta una prueba ya que se incluye antes en la validación para crear nuevo

--- app/views/UserView.js
+++ app/views/UserView.js
@@ -3,18 +3,20 @@ const UserService = require('../services/UserService')
 
 class UserView{
     static createUser(payload){
-        // if(payload==null)
-        // {
-        //     return {error:"payload no existe"}
-        // }
-        // else if(payload.username==null || payload.name==null||payload.id==null){
-        //     return {error:"Necesitan tener un valor válido, ningun valor puede ser nulo"}            
-        // }
-        // else if( typeof payload.username==='string' ||typeof payload.name === 'string' || typeof payload.id === 'number'){
-        //     return {error:"Necesitan tener un valor válido"}            
-        // }else{
-            return UserService.create(payload.id, payload.username, payload.name)
-        // }
+        if(payload===null)
+        {
+            return {error:"payload no existe"}
+        }
+        else if(payload.username===null || payload.name===null||payload.id===null){
+            return {error:"Necesitan tener un valor válido, ningun valor puede ser nulo"}            
+        }
+        else if( typeof payload.username==='string' ||typeof payload.name === 'string' || typeof payload.id === 'number'){
+            return UserService.create(payload.id, payload.username, payload.name)   
+        }else{
+            return {error:"Necesitan tener un valor válido"}
+           
+        }
+    
 
     
 
--- test/views/UserView.test.js
+++ test/views/UserView.test.js
@@ -1,18 +1,18 @@
 const UserView = require('./../../app/views/UserView')
 
 describe("Test for UserView", () =>{
-    // test("Return an error object when try to create a new user with an null payload", () =>{
-    //     const payload = null
-    //     const result = UserView.createUser(payload)
-    //     expect(result.error).toMatch(/payload no existe/);
+    test("Return an error object when try to create a new user with an null payload", () =>{
+        const payload = null
+        const result = UserView.createUser(payload)
+        expect(result.error).toMatch(/payload no existe/);
 
-    // });
+    });
 
-    // test("Return an error object when try to create a new with a payload with invalid properties", ()=>{
-    //     const payload = {username:null, name:12, id:"id"}
-    //     const result= UserView.createUser(payload)
-    //     expect(result.error).toMatch(/Necesitan tener un valor válido, ningun valor puede ser nulo/)
-    // })
+    test("Return an error object when try to create a new with a payload with invalid properties", ()=>{
+        const payload = {username:null, name:12, id:"id"}
+        const result= UserView.createUser(payload)
+        expect(result.error).toMatch(/Necesitan tener un valor válido, ningun valor puede ser nulo/)
+    })
 
     // test("Return an error object when try to create a new with a new user with a patload with missing properties", ()=>{
     //     const payload = {username:"Username"}
